93ff7710666eee90a58c0d67667d952fb7df9b04
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-08-10 12:03:13 +0530

    Update TodoList.jsx

--- TodoList.jsx
+++ TodoList.jsx
@@ -1,40 +1,55 @@
-import {useState} from 'react';
+import {useState} from 'React';
+import { v4 as uuidv4 } from "uuid";
+
+
 export default function TodoList() {
    
-let [Todos, setTodos]= useState(["sample task"]);
+let [Todos, setTodos]= useState([{ task: "sample-task", id: uuidv4() }]);
    
 let[newTodos, setnewTodos]= useState("");
 
    
 let addnewTask = ()=> {
-  setTodos([...Todos, newTodos]);
-  setnewTodos("");
+  setTodos ((prevTodos) => {
+return [...prevTodos, {task: newTodos, id: uuidv4() }];
+});
+setNewTodos ("");
+}
+
+
+
+let updateTodoValue = (event) => { setnewTodos(event.target.value);
 };
 
-let updateTodoValue = (event) => {
-   setnewTodos(event.target.value);
+
+let deleteTodo =(id)=> {
+setTodos((prevTodos)=>Todos.filter((prevTodos) => prevTodos.id != id));
 };
 
+   
  return (
    <div>
      <input placeholder="enter the todo"value={newTodos} onChange={updateTodoValue}></input>
       <br></br>
-<button onClick={addnewTask}> submit</button>
+     <button onClick={addnewTask}> submit</button>
 
       <br></br> <br></br>
-
       <hr></hr>
+      
       <h4>Todo task</h4>
 
-      <ul> {
-         todos.map((todo)=>{
-            <li>{todo}</li>
-         })
-      }</ul>
+   <ul>
+      {Todos.map((todo) => (
+      <li key={todo.id}>
+      <span> {todo.task}</span>
+      &nbsp;&nbsp;&nbsp;
+      <button onClick={()=> deleteTodo(todo.id)}>delete</button>
       
-
-      
+      </li>
+      ))}
+    
+   </ul>
+          
    </div>
- );
-
+ );                   
 }
